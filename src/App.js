2b072b0ce6284ad2a6ec5aae1260e33b5a6707a1
L200160149 <ums.informatika@gmail.com>
2022-05-18 09:06:57 +0700

    card component

--- src/App.js
+++ src/App.js
@@ -27,6 +27,7 @@ function App() {
   return (
     <div className="App">
       <h1>React</h1>
+
       <ExpenseItem title={expenses[0].title} amount={expenses[0].amount} date={expenses[0].date}></ExpenseItem>
       <ExpenseItem title={expenses[1].title} amount={expenses[1].amount} date={expenses[1].date}></ExpenseItem>
       <ExpenseItem title={expenses[2].title} amount={expenses[2].amount} date={expenses[2].date}></ExpenseItem>
